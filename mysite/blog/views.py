fc54e94bf030861109b1bab8eab96f2bbac086e2
Chirath02 <chirath.02@gmail.com>
2017-06-06 17:08:16 +0530

    day 7

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
+from django.views import generic
 from blog.models import Post, Comment
 
 
@@ -21,8 +24,27 @@ def post_detail(request, pk):
     return render(request, template_name, context)
 
 
+def add_post(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        img = request.FILES['img']
+        is_published = request.POST['is_published']
+        user = User.objects.get(username=request.user.username)
+        new_post = Post(title=title, user=user, content=content, img=img, is_published=is_published)
+        new_post.save()
+        return redirect('post', new_post.id)
+    else:
+        template_name = 'blog/add_post.html'
+        context = {}
+        return render(request, template_name, context)
+
+
 def edit_post(request, pk):
     post = Post.objects.get(id=int(pk))
+    if request.user.username != post.user.username:
+        # permission denied
+        raise PermissionDenied
     if request.method == "GET":
         template_name = 'blog/post_edit.html'
         context = {'post': post}
@@ -30,7 +52,75 @@ def edit_post(request, pk):
     else:
         post.title = request.POST['title']
         post.content = request.POST['content']
-        post.is_published = request.POST['is_published']
-        post.img = request.FILES['img']
+        if 'img' in request.FILES:
+            post.img = request.FILES["img"]
+        if 'is_published' in request.POST:
+            post.is_published = request.POST['is_published']
         post.save()
         return redirect('post', post.id)
+
+
+def delete_post(request, id):
+    if Post.objects.get(id=id) is not None:
+        post = Post.objects.get(id=id)
+        if post.user.username == request.user.username:
+            post.delete()
+            return redirect('home')
+    raise PermissionDenied
+
+
+class DeletePostView(generic.DeleteView):
+    model = Post
+    success_url = '/'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class AddPost(generic.CreateView):
+    model = Post
+    template_name = 'blog/add_post.html'
+    success_url = '/blog'
+    fields = ['title', 'content', 'img', 'is_published', 'user']
+
+
+class EditPost(generic.UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'content', 'img', 'is_published', 'user']
+    success_url = '/blog'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
